eac69ec83a58b6024612ed1264eb05c026ec7241
Bimochan Shrestha <bmochan@gmail.com>
2020-03-11 13:16:43 +0545

    project-structure: Implement API calls, class to className

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,16 +1,41 @@
 import React, {Component} from 'react';
 import MediaColumn from './MediaColumn';
+import { fetchTrendingMedia } from 'src/services/apiServices';
 
 class Dashboard extends Component {
   constructor(props) {
     super(props);
-    this.state = {}
+    this.state = {
+      timeWindow: 'day',
+      movies: {},
+      shows: {}
+    }
+  }
+  fetchMovies = async () => {
+    try {
+      const movieData = await fetchTrendingMedia('movie',this.state.timeWindow);
+      console.log(movieData);
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  fetchShows = async () => {
+    try {
+      const movieData = await fetchTrendingMedia('tv',this.state.timeWindow);
+      console.log(movieData);
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  componentDidMount() {
+    Promise.all([this.fetchMovies(),this.fetchShows()])
   }
 
   render() {
 
     return (
-      <div class="container">
+      <div className="container">
         <MediaColumn/>
         <MediaColumn/>
       </div>
--- src/services/apiServices.js
+++ src/services/apiServices.js
@@ -1,13 +1,9 @@
 import http from '../utils/http';
 import { projectConfig } from '../projectConfig';
-import { interpolate } from '../utils/interpolate';
 
 
 export async function fetchTrendingMedia(mediaType, timeWindow) {
-  const url = interpolate(projectConfig.trendingUrl, {
-    mediaType: mediaType,
-    timeWindow: timeWindow
-  });
+  const url = `${projectConfig.trendingUrl}/${mediaType}/${timeWindow}`
   const response = await http.get(url);
 
   return response;
