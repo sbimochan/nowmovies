e29a85ebfd874ca44b114ec9cca6ce367d3d97b1
Bimochan Shrestha <bmochan@gmail.com>
2020-03-11 17:53:54 +0545

    Updates

---
+++ precache-manifest.2212899ef14c07bb967022a141c02bd0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23f7d7771047acaa1e354930d3bf4c7b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb70c590f385243f8660",
+    "url": "/static/css/main.6575e4ca.chunk.css"
+  },
+  {
+    "revision": "62952687078ddd895b91",
+    "url": "/static/js/2.9e0336c1.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.9e0336c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb70c590f385243f8660",
+    "url": "/static/js/main.644eadc6.chunk.js"
+  },
+  {
+    "revision": "4946dc28439b7282baad",
+    "url": "/static/js/runtime-main.e5aeda99.js"
+  }
+]);
