2b99edc248b0ecd97d60a4b1ecb0207cef9050f1
Bimochan Shrestha <bmochan@gmail.com>
2020-06-19 19:12:53 +0545

    Optional chaaining

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -3,10 +3,10 @@ export const textTruncate = (text, length, ending) => {
     length = 100;
   }
   if (ending === null) {
-    ending = '...';
+    ending = "...";
   }
-  if (text.length > length) {
-    return text.substring(0, length - ending.length) + ending;
+  if (text?.length > length) {
+    return text.substring(0, length - ending?.length) + ending;
   } else {
     return text;
   }
